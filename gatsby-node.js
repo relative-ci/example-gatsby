06d1449467fb048f2cee8547cb99b801814fb794
Vio <vio@beanon.com>
2020-03-09 08:23:18 +0100

    build: Setup relative-ci

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { RelativeCiAgentWebpackPlugin } = require("@relative-ci/agent")
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage === "build-javascript") {
+    actions.setWebpackConfig({
+      plugins: [new RelativeCiAgentWebpackPlugin()],
+    })
+  }
+}
